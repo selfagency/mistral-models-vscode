80f759d28ce330a644d6a4a9d9728060e19cfc64
Daniel Sieradski <daniel@self.agency>
2026-03-04 15:25:31 -0500

    fix: add js import extensions

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { MistralChatModelProvider } from './provider';
+import { MistralChatModelProvider } from './provider.js';
 
 export function activate(context: vscode.ExtensionContext) {
   const logOutputChannel =
